4773efd6e27e0a8761e739fa35de28414f3a1b18
insoo00 <insoosos1234@gmail.com>
2024-10-07 14:32:52 +0900

    @Parallel Routes

--- next/next-practice/app/page.tsx
+++ next/next-practice/app/page.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
     <div>
       <h2>홈페이지</h2>
-      <p>Next.j 페이지입니다</p>
-      <Link href='/hello'>Hello</Link>
-      <br />
-      <Link href='/date'>Date</Link>
     </div>
   );
 }
